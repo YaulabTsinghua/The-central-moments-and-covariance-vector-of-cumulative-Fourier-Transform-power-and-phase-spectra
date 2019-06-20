abf41416a9fefa606086574d4cc490a4010eabd4
YaulabTsinghua <38828609+YaulabTsinghua@users.noreply.github.com>
2019-06-20 10:11:23 +0800

    Add files via upload

---
+++ Fourier_ang_C.m
@@ -0,0 +1,39 @@
+T_r=fastaread('Influenza.fasta');
+n0=size(T_r,1);
+N=cell(4,1);
+A=zeros(1,n0);
+tic
+for d=1:n0
+    n=size(T_r(d).Sequence,2);
+    M=zeros(4,n);
+    N{1}=strfind(upper(T_r(d).Sequence),'A');
+    N{2}=strfind(upper(T_r(d).Sequence),'C');
+    N{3}=strfind(upper(T_r(d).Sequence),'G');
+    N{4}=strfind(upper(T_r(d).Sequence),'T'); 
+    M(1,N{1})=1;
+    M(2,N{2})=1;
+    M(3,N{3})=1;
+    M(4,N{4})=1;
+    F=fft(M')';
+    P=abs(F).^2;
+    ang=angle(F);
+    ang(ang<0)=ang(ang<0)+2*pi;
+    t=sum(M,2);
+    for k=2:n
+    ang(:,k)=sum(ang(:,max(k-1,1):k),2);
+    P(:,k)=sum(P(:,max(k-1,2):k),2);    
+    end
+    A(1:4,d)=sum(P(:,2:n),2)/(n-1);
+    A(5:8,d)=sum(abs(P(:,2:n)-A(1:4,d)*ones(1,n-1)),2)/n;
+    A(9:12,d)=sum((P(:,2:n)-A(1:4,d)*ones(1,n-1)).^2,2)./(n^2*(t*n-t.^2));
+    A(13,d)=sum(abs(P(1,2:n)-A(1,d)).*abs(P(2,2:n)-A(2,d)))/(n^2*(t(1)+t(2))*(n-(t(1)+t(2))/2)/2);
+    A(14,d)=sum(abs(P(1,2:n)-A(1,d)).*abs(P(3,2:n)-A(3,d)))/(n^2*(t(1)+t(3))*(n-(t(1)+t(3))/2)/2);
+    A(15,d)=sum(abs(P(1,2:n)-A(1,d)).*abs(P(4,2:n)-A(4,d)))/(n^2*(t(1)+t(4))*(n-(t(1)+t(4))/2)/2);
+    A(16,d)=sum(abs(P(2,2:n)-A(2,d)).*abs(P(3,2:n)-A(3,d)))/(n^2*(t(2)+t(3))*(n-(t(2)+t(3))/2)/2);
+    A(17,d)=sum(abs(P(2,2:n)-A(2,d)).*abs(P(4,2:n)-A(4,d)))/(n^2*(t(2)+t(4))*(n-(t(2)+t(4))/2)/2);
+    A(18,d)=sum(abs(P(3,2:n)-A(3,d)).*abs(P(4,2:n)-A(4,d)))/(n^2*(t(3)+t(4))*(n-(t(3)+t(4))/2)/2);
+    A(19:22,d)=sum(ang(:,2:n),2)/(n-1);    
+end
+toc
+%save('Fourier_accu_HIV.mat','A');
+
